45dc5099839283d3a66e700a0e07c52406f00748
bihaosky2024 <bihao.sky@gmail.com>
2025-03-23 18:35:08 +0800

    Add files via upload

--- init_model.py
+++ init_model.py
@@ -2,10 +2,11 @@ from langchain_openai import ChatOpenAI
 from langchain.chat_models import init_chat_model
 
 
-def init_openai_model(api_key, model_name="gpt-4o-mini"):
+def init_openai_model(api_key, model_name="gpt-4o-mini", temperature=0.1):
 
     model = ChatOpenAI(model=model_name,
                        openai_api_key=api_key,
+                       temperature=temperature,
                        openai_api_base="https://api.aigc369.com/v1")
     return model
     
