76e18cbf42b195cd915d9d55b83ded8dae5967a5
bihaosky2024 <bihao.sky@gmail.com>
2025-03-23 20:31:11 +0800

    Add files via upload

--- backend.py
+++ backend.py
@@ -1,29 +1,31 @@
-from init_model import init_openai_model
-
-from langchain_core.messages import HumanMessage
+from openai import OpenAI
 import base64
 
 # 读取图像并编码为 Base64
 def encode_image(image_path):
     with open(image_path, "rb") as f:
         return base64.b64encode(f.read()).decode("utf-8")
-    
-def ocr_red_box(image_path, api_key):
+
+
+def ocr_assitant(image_path, api_key, red_box=False):
+
     image_data = encode_image(image_path)
-
-    llm = init_openai_model(api_key, model_name="gpt-4o")
-    message = HumanMessage(
-        content=[
-            {"type": "text", "text": "extract text in the red box only in this image, do not output any other information."},
-            {
-                "type": "image_url",
-                "image_url": {"url": f"data:image/jpeg;base64,{image_data}"},
-            },
-        ],
+    client = OpenAI(
+        api_key=api_key,
+        base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
     )
-    response = llm.invoke([message])
+    prompt_ls = [
+        "Extract text in this image, do not output any other information.",
+        "Extract text in the red box only in this image, do not output any other information.",
+    ]
     
-    return response.content
-
-
-
+    completion = client.chat.completions.create(
+        model="qwen-vl-max",  
+        messages=[{"role": "user","content": [
+                {"type": "text", "text": prompt_ls[red_box]},
+                {"type": "image_url",
+                "image_url": {"url": f"data:image/jpeg;base64,{image_data}"}}
+                ]}]
+        )
+    
+    return completion.choices[0].message.content 
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import streamlit as st
-from backend import ocr_red_box
+from backend import ocr_assitant
 from utils import save_temp_file
 
 
 st.title("🖊️ OCR文字识别小助手")
-st.markdown("##### 基于OpenAI GPT4o的AI小助手：\n##### 输入图片，并使用红色方框标记出需要识别的文字区域。")
+st.markdown("##### 基于通义千问视觉模型的AI小助手：\n##### 输入图片，并使用红色方框标记出需要识别的文字区域。")
 
 with st.sidebar:
-    openai_api_key = st.text_input("请输入OpenAI API密钥：", type="password")
-    st.markdown("[获取OpenAI API密钥](https://platform.openai.com/account/api-keys)")
+    api_key = st.text_input("请输入通义千问 API密钥：", type="password")
+    st.markdown("[获取通义千问 API密钥](https://bailian.console.aliyun.com/)")
 
 
 uploaded_file = st.file_uploader("上传你的图片（使用红色方框标记出需要识别的文字区域）：", type=["png", "jpg", "jpeg"])
@@ -18,13 +18,14 @@ if not uploaded_file:
 temp_file_path = "temp"
 save_temp_file(temp_file_path, uploaded_file)
 
+option = st.selectbox("请选择需要文字识别的区域：", ["全图识别", "自定义识别（红色矩形框标记）"], index=0)
 submit = st.button("开始识别")
 if submit:
-    if not openai_api_key:
-        st.info("请先输入OpenAI API密钥")
+    if not api_key:
+        st.info("请先输入通义千问 API密钥")
         st.stop()
     else:
         with st.spinner("正在识别..."):
-            result = ocr_red_box(temp_file_path, openai_api_key)
+            result = ocr_assitant(temp_file_path, api_key, red_box=(option == "自定义识别（红色矩形框标记）"))
             st.success("识别结果：")
             st.write(result)
